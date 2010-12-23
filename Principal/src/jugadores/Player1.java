77588134691af6b0d7eae64d29f5f01cee53cc57
maekos <maekos@6cc56387-c710-2f70-5778-f6faec9ae97a>
2010-12-23 19:14:21 +0000

    Implementacion del teclado para mover el tanque y reacomodamiento de variables y metodos

--- Principal/src/jugadores/Player1.java
+++ Principal/src/jugadores/Player1.java
@@ -1,13 +1,33 @@
 
 package jugadores;
 
-public class Player1 extends Player {
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+public class Player1 extends Player{
+    private final int posIniX = 280;
+    private final int posIniY = 520;
     String color;
+    private ImageIcon img;
     public Player1(){
         color = "yellow";
+        img = new ImageIcon("tanque"+Tanque.getDireccion()+".gif");
     }
 
     public String getColor(){
         return color;
+    }
+
+    public int getPosIniX(){
+        return posIniX;
+    }
+
+    public int getPosIniY(){
+        return posIniY;
+    }
+
+    public void dibujar(Graphics g, int posX, int posY){
+        img = new ImageIcon("tanque"+Tanque.getDireccion()+".gif");
+        img.paintIcon(null, g, posX, posY);
     }
 }
